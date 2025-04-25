8c6d01fe2fdf761123db249c13b49bdb22a08547
yo-saku <mwam2000_gaugau@yahoo.co.jp>
2025-04-26 07:07:17 +0900

    ・allureReportサーバ用にコンテナを追加 ・テスト対象のURLを動的に変更できるよう修正

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -1,7 +1,7 @@
 SuiteOf('スモークテスト');
 
 Scenario('Web サイトを開きログインする ', ({ I }) => {
-    I.amOnPage('http://localhost:8080')
+    I.amOnPage('/')
     I.click('ログインする')
     I.fillField('ユーザー名', 'user1')
     I.fillField('パスワード', 'super-strong-passphrase')
